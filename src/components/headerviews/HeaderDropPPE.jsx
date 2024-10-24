dc550bae5d7a0c954f6b8bdae8cb91149cea39e8
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-24 10:55:52 -0400

    Update HeaderDropPPE.jsx

--- src/components/headerviews/HeaderDropPPE.jsx
+++ src/components/headerviews/HeaderDropPPE.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import images from '../../utils/tbsImages';
 function Header() {
     const [isNavOpen, setIsNavOpen] = useState(false);
 
@@ -10,7 +10,7 @@ function Header() {
     return (
         <header className="header">
     <a className="header-logo" href="/">
-        <img alt="TBS logo" className="tbs-logo-img" src="../public/tbs_companies/TBS logo1.png" />
+    <img alt="TBS logo" className="tbs-logo-img" src={images["../assets/tbs_companies/tbs.svg"].default} />
     </a>
     <div className="mobile-nav-icon">
     <button className="mobile-nav" onClick={toggleMenu}>
@@ -20,16 +20,16 @@ function Header() {
     <nav className={`main-nav ${isNavOpen ? 'active' : ''}`}>
         <ul className="main-nav-list">
             <li><a className="main-nav-link" href="/trafficcontrol">Traffic Control</a></li>
-            <li><a className="main-nav-link" href="/trafficplanning">Traffic Control Plans</a></li>
+            <li><a className="main-nav-link-view" href="">Traffic Control Plans</a></li>
             <li><a className="main-nav-link" href="/bollardswheels">Bollards & Wheel Stops</a></li>
             <li><a className="main-nav-link" href="/signs">Traffic Sign Manufacturing</a></li>
-            <li><a className="main-nav-link-view" href="">PPE Sales</a></li>
+            <li><a className="main-nav-link" href="/ppe">PPE Sales</a></li>
             <li><a className="main-nav-link" href="/rentals">Equipment Rental & Sales</a></li>
         </ul>
     </nav>
     <div className="header-worx-links">
-    <a className="header-worx-logo" href="/materialworx">
-        <img className="material-worx-img"alt="Material WorX logo" src="../public/tbs_companies/material worx.png"/>
+    <a className="header-worx-logo" target="_blank" rel="noopener noreferrer" href="https://www.material-worx.com">
+        <img className="material-worx-img"alt="Material WorX logo" src={images["../assets/tbs_companies/Material WorX.svg"].default} />
         <h1 className="material-worx-text">Custom Shop</h1>
     </a>
     </div>
